0e674776b0e56d03518160c33580d6fd37f51b02
Jeferson Daniel <jeferson.daniel412@gmail.com>
2016-03-12 21:10:09 -0300

    Updating test configuration

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker-compose start && docker-compose run --rm -e BASE_URL=web:8000 web behave $@
+docker-compose start db && sleep 3
+docker-compose start web && sleep 3
+docker-compose run --rm -e BASE_URL=web:8000 web behave $@
