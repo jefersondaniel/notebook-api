da0910afe16212ecce2646e85d239abd89d594e5
Jeferson Daniel <jeferson.daniel412@gmail.com>
2016-03-12 21:22:39 -0300

    Updating .travis.yml

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-docker-compose start db && sleep 3
-docker-compose start web && sleep 3
-docker-compose run --rm -e BASE_URL=web:8000 web behave $@
+docker-compose start && docker-compose run --rm -e BASE_URL=web:8000 web behave $@
